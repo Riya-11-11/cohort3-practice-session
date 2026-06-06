ce396c8e13c07914326e2d12bd57d75e1f485bf5
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-06 21:23:06 +0530

    Refactor frequency counting logic to use a count object instead of an array

--- JS-practice/most-freq-num.js
+++ JS-practice/most-freq-num.js
@@ -1,19 +1,19 @@
 let arr = [1, 2, 3, 2, 4, 2, 5, 1, 1, 1];
 
-let map = [];
+let count = {};
 let max = 0;
 let ans;
 
 for (let i = 0; i < arr.length; i++) {
   let val = arr[i];
-  if (map[val]) {
-    map[val]++;
+  if (count[val]) {
+    count[val]++;
   } else {
-    map[val] = 1;
+    count[val] = 1;
   }
 
-  if (map[val] > max) {
-    max = map[val];
+  if (count[val] > max) {
+    max = count[val];
     ans = val;
   }
 }
