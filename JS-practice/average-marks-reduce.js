a64bdea843b0c101cb8d70418cad21a8598e26a7
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-06 21:02:19 +0530

    Add comment to clarify initial value in reduce function for average calculation

--- JS-practice/average-marks-reduce.js
+++ JS-practice/average-marks-reduce.js
@@ -2,7 +2,7 @@ let marks = [80, 90, 70, 85, 95];
 
 let total = marks.reduce(function (acc, curr) {
   return acc + curr;
-}, 0);
+}, 0); //0 is set initially to acc
 
 let avg = total / marks.length;
 console.log(avg);
