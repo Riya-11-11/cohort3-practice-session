19d651ec5558dcba14ee690e7aaba7be97a8a948
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-06 21:02:33 +0530

    Rename filter function parameter for clarity

--- JS-practice/find-expensive-product-filter.js
+++ JS-practice/find-expensive-product-filter.js
@@ -2,8 +2,8 @@
 
 let arr = [100, 250, 500, 150, 700];
 
-let ans = arr.filter(function (elem) {
-  return elem > 300;
+let ans = arr.filter(function (price) {
+  return price > 300;
 });
 
 console.log(ans);
