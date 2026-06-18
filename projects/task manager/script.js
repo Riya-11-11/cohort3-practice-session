f7b3b3fb604618d59ccb154248cb0612c28cd121
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-18 12:11:46 +0530

    Refactor task list functionality and update styles for task completion toggle

--- projects/task manager/script.js
+++ projects/task manager/script.js
@@ -2,7 +2,7 @@ let themeToggleBtn = document.querySelector("#theme-toggle");
 
 let totalTask = document.querySelector("#total-tasks");
 let pendingTask = document.querySelector("#pending-tasks");
-let completedTask = document.querySelector("#pending-tasks");
+let completedTask = document.querySelector(".completed-tasks");
 
 let addTaskInp = document.querySelector("#task-input");
 let addTaskBtn = document.querySelector("#add-task-button");
@@ -13,3 +13,49 @@ let completeToggleBtn = document.querySelector("#complete-toggle");
 
 let editBtn = document.querySelector(".edit-button");
 let deleteBtn = document.querySelector(".delete-button");
+
+let taskListContainer = document.querySelector(".task-list-container");
+let taskList = document.querySelector(".task-list");
+
+addTaskBtn.addEventListener("click", () => {
+  let taskInpVal = addTaskInp.value;
+
+  if (taskInpVal.trim() === "") {
+    alert("Enter something to add task!!!");
+    return;
+  }
+
+  taskListContainer.innerHTML += `
+   <div class="task-list">
+        <button class="complete-toggle"></button>
+        <h3>${taskInpVal}</h3>
+        <div class="task-actions">
+          <button class="edit-button">Edit</button>
+          <button class="delete-button">Delete</button>
+        </div>
+    </div>`;
+
+  addTaskInp.value = "";
+
+  totalTask.textContent++;
+  pendingTask.textContent++;
+});
+
+taskListContainer.addEventListener("click", (e) => {//event delegation
+  // console.log(e.target.className);
+
+  if (e.target.className === "delete-button") {
+    e.target.closest(".task-list").remove(); //e.target.parentElement.parentElement.remove();
+
+    totalTask.textContent--;
+    pendingTask.textContent--;
+  }
+
+  if (e.target.className==="complete-toggle") {
+    taskList.style.backgroundColor = "green";
+    taskList.style.textDecoration = "line-through";
+
+    completedTask.textContent++;
+    pendingTask.textContent--;
+  }
+});
