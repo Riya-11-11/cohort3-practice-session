9ad7f10662019e4a0ac00fdf32de633c205b6205
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-18 10:14:18 +0530

    Add progress header and track elements to the task manager interface

---
+++ projects/task manager/script.js
@@ -0,0 +1,15 @@
+let themeToggleBtn = document.querySelector("#theme-toggle");
+
+let totalTask = document.querySelector("#total-tasks");
+let pendingTask = document.querySelector("#pending-tasks");
+let completedTask = document.querySelector("#pending-tasks");
+
+let addTaskInp = document.querySelector("#task-input");
+let addTaskBtn = document.querySelector("#add-task-button");
+
+let priorityOptions = document.querySelector("#priority-select");
+
+let completeToggleBtn = document.querySelector("#complete-toggle");
+
+let editBtn = document.querySelector(".edit-button");
+let deleteBtn = document.querySelector(".delete-button");
